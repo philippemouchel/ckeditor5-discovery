6539327c096603d14df38c89ceed45205b7d3f54
Philippe Mouchel <philippe.mouchel@alithya.com>
2023-08-11 15:32:21 -0400

    Add more features in the CKEditor toolbar: Headings, HTML source, Underline and Strike + ignore dist directory.

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Bold, Underline, Strikethrough, Italic } from '@ckeditor/ckeditor5-basic-styles';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
+import { Heading } from '@ckeditor/ckeditor5-heading';
 
 ClassicEditor
     .create( document.querySelector( '#editor' ), {
-        plugins: [ Essentials, Paragraph, Bold, Italic ],
-        toolbar: [ 'bold', 'italic' ]
+        plugins: [
+            Essentials, Paragraph, Heading, SourceEditing,
+            Bold, Underline, Strikethrough, Italic,
+        ],
+        toolbar: [
+            'bold', 'underline', 'strikethrough', 'italic',
+            'heading', 'sourceEditing',
+        ],
+        heading: {
+            options: [
+                { model: 'heading2', view: 'h2', title: 'Heading 2' },
+                { model: 'heading3', view: 'h3', title: 'Heading 3' },
+                { model: 'heading4', view: 'h4', title: 'Heading 4' },
+                { model: 'paragraph', title: 'Paragraph' },
+            ]
+        }
     } )
     .then( editor => {
         console.log( 'Editor was initialized', editor );
