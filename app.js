9619ea3e881acc5f69e78de43bea4ea77a6e63a2
Philippe Mouchel <philippe.mouchel@alithya.com>
2023-08-11 16:17:59 -0400

    PRovide a very basic timestamp plugin with a button to add current date in editor.

--- app.js
+++ app.js
@@ -6,15 +6,52 @@ import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { List } from '@ckeditor/ckeditor5-list';
 import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
 
+import { Plugin } from '@ckeditor/ckeditor5-core';
+import { ButtonView } from '@ckeditor/ckeditor5-ui';
+
+class Timestamp extends Plugin {
+    init() {
+        const editor = this.editor;
+        // The button must be registered among the UI components of the editor
+        // to be displayed in the toolbar.
+        editor.ui.componentFactory.add( 'timestamp', () => {
+            // The button will be an instance of ButtonView.
+            const button = new ButtonView();
+            button.set( {
+                label: 'Ts',
+                withText: true
+            } );
+
+            // Execute a callback function when the button is clicked.
+            button.on( 'execute', () => {
+                const now = new Date();
+
+                // Change the model using the model writer.
+                editor.model.change( writer => {
+
+                    // Insert the text at the user's current position.
+                    editor.model.insertContent( writer.createText( now.toString() ) );
+                } );
+            } );
+
+            return button;
+        } );
+    }
+}
+
 ClassicEditor
     .create( document.querySelector( '#editor' ), {
         plugins: [
             Essentials, Heading, Paragraph, List, SourceEditing,
             Bold, Underline, Strikethrough, Italic,
+            Timestamp,
         ],
         toolbar: [
+            'sourceEditing', '|',
             'bold', 'underline', 'strikethrough', 'italic', '|',
-            'heading', '|', 'bulletedList', 'numberedList', '|', 'sourceEditing',
+            'heading', '|',
+            'bulletedList', 'numberedList', '|',
+            'timestamp',
         ],
         heading: {
             options: [
